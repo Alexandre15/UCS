cf7c55051108a0ce577305c0c8a4e00a114adb6e
Alexandre15 <alefagundes155@gmail.com>
2024-12-02 18:52:10 -0300

    RQ3-2

--- vetores/revisaoQ3.c
+++ vetores/revisaoQ3.c
@@ -3,21 +3,37 @@
 #define TAMANHO 6
 
 int main(){
-    int idx, vacinas[TAMANHO], corte;
+    int idx, idx2, vacinas[TAMANHO], corte, aux;
 
     for (idx = 0; idx < TAMANHO; idx++)
     {
         printf("Digite a data do Lote %d: ", idx+1);
         scanf("%d", &vacinas[idx]);
     }
-    printf("Digite a data de corte: ");
+    printf("\nDigite a data de corte: ");
     scanf("%d", &corte);
     
     for (idx = 0; idx < TAMANHO; idx++)
     {
-        printf("Vacinas: %d\n", vacinas[idx]);
+        for (idx2 = 0; idx2 < TAMANHO-1; idx2++)
+        {
+            if (vacinas[idx2]>vacinas[idx2+1])
+            {
+                aux=vacinas[idx2+1];
+                vacinas[idx2+1]=vacinas[idx2];
+                vacinas[idx2]=aux;
+            }
+            
+        }
+        
     }
-    tainha
 
 
+    for (idx = 0; idx < TAMANHO; idx++)
+    {
+        if (vacinas[idx]<=corte)
+        {
+            printf("\nData de Corte %d: %d\n", idx+1, vacinas[idx]);
+        }
+    }
 }
