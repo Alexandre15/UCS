0db0bc24cdc17bdf687dfa969a47313c2ca36fd0
Alexandre Fagundes <alefagundes155@gmail.com>
2025-07-06 13:42:57 -0300

    ucs

--- Prog2/revisão/gpt/arquivos/desafio3.c
+++ Prog2/revisão/gpt/arquivos/desafio3.c
@@ -2,34 +2,30 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct{
     char *nome;
-} Pessoas;
-
+    int idade;
+    char cpf[12];
+} Pessoa;
 
 int main(){
-    int i;
-    int quantidade;
-    char buffer[200];
-    int memoria = 0;
+    int i, quantidade, memoria = 0;
+    char buffer[100];
 
-    printf("Quantidades de pessoas: ");
+    printf("Quantas pessoas deseja cadastrar: ");
     scanf("%d", &quantidade);
     getchar();
 
-    Pessoas *p = malloc(quantidade * sizeof(Pessoas));
+    Pessoa *p = malloc(quantidade * sizeof(Pessoa));
     if (p == NULL)
     {
-        printf("Erro ao alocar vetor Pessoas.\n");
-        return 1;
+        printf("Erro ao alocar vetor Pessoa.\n");
     }
     
-
     for (i = 0; i < quantidade; i++)
     {
         printf("Nome %d: ", i + 1);
         fgets(buffer, sizeof(buffer), stdin);
-
         buffer[strcspn(buffer, "\n")] = '\0';
 
         p[i].nome = malloc(strlen(buffer) + 1);
@@ -39,19 +35,28 @@ int main(){
             return 1;
         }
         strcpy(p[i].nome, buffer);
-        memoria += (strlen(p[i].nome) + 1);
-        
+
+        printf("Idade: ");
+        scanf("%d", &p[i].idade);
+        getchar();
+
+        printf("CPF: ");
+        fgets(p[i].cpf, sizeof(p[i].cpf), stdin);
+        p[i].cpf[strcspn(p[i].cpf, "\n")] = '\0';
+        getchar();
+        memoria += (strlen(p[i].nome) + 1) + 16;
+
     }
-    printf("--- Nomes cadastrados ---\n");
+    printf("--- Pessoas cadastradas ---\n");
     for (i = 0; i < quantidade; i++)
     {
-        printf("Nome: %s\n", p[i].nome);
+        printf("Nome %d = %s\n", i + 1, p[i].nome);
+        printf("Idade: %d\n", p[i].idade);
+        printf("CPF = %s\n", p[i].cpf);
+        free(p[i].nome);
     }
-    printf("Memoria usado de %d Bytes", memoria);
-    
-    for (i = 0; i < quantidade; i++) {
-        free(p[i].nome); // libera cada nome individual
-    }
+
     free(p);
+    printf("Memoria ocupada = %d", memoria);
     return 0;
 }
